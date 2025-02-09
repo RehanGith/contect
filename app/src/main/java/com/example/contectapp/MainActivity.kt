10f7b6da7f6a3b31c24ae1bc1442f79fe54e6ac4
RehanGith <chrehan1007@gmail.com>
2025-02-09 14:20:39 +0500

    Created functionality to appear contact diaplog

--- app/src/main/java/com/example/contectapp/MainActivity.kt
+++ app/src/main/java/com/example/contectapp/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.contectapp
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,6 +31,34 @@ class MainActivity : AppCompatActivity() {
         binding.rvContactsList.layoutManager = LinearLayoutManager(this)
         binding.rvContactsList.setHasFixedSize(true)
         binding.rvContactsList.setItemViewCacheSize(10)
+        binding.btAdd.setOnClickListener {
+            displayAddContactDialog()
+        }
+    }
 
+    @SuppressLint("InflateParams")
+    private fun displayAddContactDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(
+            R.layout.dialog_add_contect,
+            null
+        )
+
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setCancelable(true)
+            .create()
+
+
+        dialogView.findViewById<Button>(R.id.btnAddContact).setOnClickListener {
+            val name = dialogView.findViewById<EditText>(R.id.etFirstName).text.toString()
+            val phone = dialogView.findViewById<EditText>(R.id.etPhoneNumber).text.toString()
+            if (name.isNotEmpty() && phone.isNotEmpty()) {
+                Toast.makeText(this, "Name $name and Phone $phone", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+            } else {
+                Toast.makeText(this, "Name and Phone cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+        dialog.show()
     }
 }
